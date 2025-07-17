7951d0ba666a2b4ab165bbe2528ff13067d08e95
sosl-legion <yweifu@uri.edu>
2025-07-17 12:24:35 -0400

    added full camera calibration vehicle and topside launch files and topside remote tag detection launch file

---
+++ mini_alpha_bringup/launch/bringup_camera_calibration_tag_detection_topside.launch.py
@@ -0,0 +1,27 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import IncludeLaunchDescription
+
+from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    arg_robot_name = 'mini_alpha'
+    robot_bringup = arg_robot_name + '_bringup'
+
+    camera_remote_node = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory(robot_bringup), 
+                'launch','include','camera_calibration_tag_detection_topside.launch.py')), 
+    )
+
+    return LaunchDescription([
+        camera_remote_node
+    ])
---
+++ mini_alpha_bringup/launch/include/camera_calibration_tag_detection_topside.launch.py
@@ -0,0 +1,32 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    """
+    Launch file to run the camera driver and the camera calibrator node.
+    """
+    robot_name = 'mini_alpha'
+    robot_bringup = robot_name + '_bringup'
+    
+    robot_param_path = get_package_share_directory(robot_bringup)
+
+    apriltag_detection_topside_params_path = os.path.join(robot_param_path, 'config', 'camera_calibration_tag_detection_topside.yaml')
+
+    explore_camera_remote_node = Node(
+        package='dwe_camera_driver',
+        executable='remote_node',
+        name='explore_camera_remote_node',
+        namespace=robot_name,
+        output='screen',
+        parameters=[apriltag_detection_topside_params_path],
+        remappings=[
+            ('apriltag_detection/compressed', 'apriltag_detection/image/compressed')
+        ]
+    )
+
+
+    return LaunchDescription([
+        explore_camera_remote_node,
+    ])
